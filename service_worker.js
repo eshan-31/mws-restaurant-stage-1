b799564a85e60695fec3cb09c276d759b97a154f
Eshan Singh Kachwaha <singheshan31@gmail.com>
2018-05-20 00:39:06 +0530

    service worker completed

--- service_worker.js
+++ service_worker.js
@@ -22,3 +22,27 @@ var toCache = [
   './img/9.jpg',
   './img/10.jpg',
 ];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(toCache);
+      })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+    .catch(err => console.log(err, event.request))
+  );
+});
