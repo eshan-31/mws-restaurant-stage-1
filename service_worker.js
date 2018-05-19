e16984f99aec0f9b71328209b8ddcdab2ea6e0cc
Eshan Singh Kachwaha <singheshan31@gmail.com>
2018-05-20 01:11:56 +0530

    proper indentation

--- service_worker.js
+++ service_worker.js
@@ -1,52 +1,55 @@
-self.addEventListener('install', function (event) {
-  // Perform install steps
-}); var CACHE_NAME = 'resto-cache';
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
+var CACHE_NAME = 'resto-cache';
 
 //elements to be cached
 var toCache = [
-  '/',
-  './index.html',
-  './restaurant.html',
-  './css/styles.css',
-  './js/dbhelper.js',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './data/restaurants.json',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
+    '/',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
 ];
 
 //service worker code referenced from stack overflow
 
-self.addEventListener('install', function (event) {
-  // install service worker
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(toCache);
-      })
-  );
+self.addEventListener('install', function(event) {
+    // install service worker
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(toCache);
+        })
+    );
 });
 //activate service worker
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
 });
 
 //fetch files from the cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-    .catch(err => console.log(err, event.request))
-  );
+    event.respondWith(
+        caches.match(event.request, {
+            ignoreSearch: true
+        }).then(response => {
+            return response || fetch(event.request);
+        })
+        .catch(err => console.log(err, event.request))
+    );
 });
--- service_worker_reg.js
+++ service_worker_reg.js
@@ -1,12 +1,12 @@
 /**
-*Service worker registration
-*/
+ *Service worker registration
+ */
 if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function () {
-		navigator.serviceWorker.register('service_worker.js').then(function (registration) {
-			console.log('Service Worker registered successfuly'); //if the promise resolves
-		}, function (error) { //if the promise doesn't resolve
-			console.log('Service Worker registration failed');
-		});
-	});
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('service_worker.js').then(function(registration) {
+            console.log('Service Worker registered successfuly'); //if the promise resolves
+        }, function(error) { //if the promise doesn't resolve
+            console.log('Service Worker registration failed');
+        });
+    });
 }
